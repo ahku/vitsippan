bfbf496d8a8ea21f8699633b1b70abf469351578
Yeu Kang Hua <yeukang@gmail.com>
2012-05-17 17:36:12 +0200

    Nu finns det en sajt här!

---
+++ plugins/facebook/Config/facebook.php
@@ -0,0 +1,12 @@
+<?php
+
+//app/config/facebook.php
+$config = array(
+    'Facebook' => array(
+        'appId'  => '174122339309551',
+        'apiKey' => '174122339309551',
+        'secret' => '7830bff0b110f615b40e592b0b627893',
+        'cookie' => true,
+        'locale' => 'en_US',
+    )
+);
